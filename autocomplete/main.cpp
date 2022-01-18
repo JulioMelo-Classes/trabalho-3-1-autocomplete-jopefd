e8608b0764c009ec7ffd04aa30d5bace3e7a95ff
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-18 17:35:24 -0300

    Test finding for user input matches inside database

--- autocomplete/main.cpp
+++ autocomplete/main.cpp
@@ -5,15 +5,19 @@
 #include <sstream>
 #include <string>
 
+using std::cin;
 using std::cout;
 using std::endl;
 using std::for_each;
+using std::getline;
 using std::ifstream;
+using std::lower_bound;
 using std::pair;
 using std::set;
 using std::size_t;
 using std::string;
 using std::stringstream;
+using std::upper_bound;
 using std::ws;
 
 int main(int argc, char **argv) {
@@ -38,6 +42,31 @@ int main(int argc, char **argv) {
     frequencies_words.insert(frequency_word);
   }
 
-  for_each(frequencies_words.begin(), frequencies_words.end(),
-           [](auto &p) { cout << p.first << " " << p.second << endl; });
+  string user_input;
+
+  while (getline(cin, user_input)) {
+    // const auto contains_user_input =
+    //     [](const pair<size_t, string> &frequency_word_1, const pair<size_t,
+    //     string> &frequency_word_2) {
+    //       return frequency_word_2.second.find(frequency_word_1.second) !=
+    //       string::npos;
+    //     };
+
+    // const auto first =
+    //     lower_bound(frequencies_words.begin(), frequencies_words.end(),
+    //                 pair<size_t, string>{0, user_input},
+    //                 contains_user_input);
+    // const auto last =
+    //     upper_bound(frequencies_words.begin(), frequencies_words.end(),
+    //                 pair<size_t, string>{0, user_input},
+    //                 contains_user_input);
+
+    // cout << (first == frequencies_words.begin() ? first->second : "no") <<
+    // endl;
+
+    for_each(frequencies_words.begin(), frequencies_words.end(),
+             [user_input](const auto &fw) {
+               cout << (fw.second.find(user_input) != string::npos ? "sim" : "nao") << endl;
+             });
+  }
 }
