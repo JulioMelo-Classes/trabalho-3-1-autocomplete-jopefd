6485e9e72091e398425a1bb2e7fb2643323dd0d0
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-18 09:37:27 -0300

    Test reading frequency-word pair of database file

--- autocomplete/main.cpp
+++ autocomplete/main.cpp
@@ -1,14 +1,36 @@
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
 
 using std::cout;
+using std::endl;
 using std::ifstream;
+using std::pair;
+using std::set;
+using std::size_t;
 using std::string;
+using std::stringstream;
 
 int main(int argc, char **argv) {
   ifstream database(argv[1]);
   string line;
-  while (getline(database, line)) {
-    cout << line;
+
+  getline(database, line);
+  size_t lines_number = stoi(line);
+
+  while (lines_number--) {
+    stringstream aux;
+    pair<size_t, string> frequency_word;
+
+    getline(database, line);
+
+    aux << line;
+    aux >> frequency_word.first;
+    aux << line;
+    aux >> frequency_word.second;
+
+    cout << frequency_word.first << " " << frequency_word.second << endl;
   }
 }
