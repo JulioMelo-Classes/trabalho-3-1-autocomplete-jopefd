2bb0c9ce2fceb0f52cd81acffb31ce1585d4ba57
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-19 21:53:08 -0300

    Try get matching interval of user input

--- autocomplete/main.cpp
+++ autocomplete/main.cpp
@@ -19,6 +19,7 @@ using std::size_t;
 using std::string;
 using std::stringstream;
 using std::tolower;
+using std::transform;
 using std::upper_bound;
 using std::ws;
 
@@ -29,50 +30,61 @@ int main(int argc, char **argv) {
   getline(database, line);
   size_t lines_number = stoi(line);
 
-  set<pair<size_t, string>> frequencies_words;
+  set<pair<string, size_t>> frequencies_words;
 
   while (lines_number--) {
+    size_t frequency;
+    string word;
     stringstream aux;
-    pair<size_t, string> frequency_word;
 
     getline(database, line);
 
     aux << line;
-    aux >> frequency_word.first;
-    getline(aux >> ws, frequency_word.second);
+    aux >> frequency;
+    getline(aux >> ws, word);
 
-    frequencies_words.insert(frequency_word);
+    transform(word.begin(), word.end(), word.begin(), toupper);
+
+    frequencies_words.insert({word, frequency});
   }
+
+  for (const auto &fw : frequencies_words) cout << fw.first << endl;
 
   string user_input;
 
   while (getline(cin, user_input)) {
-    // const auto contains_user_input =
-    //     [](const pair<size_t, string> &frequency_word_1, const pair<size_t,
-    //     string> &frequency_word_2) {
-    //       return frequency_word_2.second.find(frequency_word_1.second) !=
-    //       string::npos;
-    //     };
+    transform(user_input.begin(), user_input.end(), user_input.begin(),
+              toupper);
 
-    // const auto first =
-    //     lower_bound(frequencies_words.begin(), frequencies_words.end(),
-    //                 pair<size_t, string>{0, user_input},
-    //                 contains_user_input);
-    // const auto last =
-    //     upper_bound(frequencies_words.begin(), frequencies_words.end(),
-    //                 pair<size_t, string>{0, user_input},
-    //                 contains_user_input);
+    auto not_contains = [](const pair<string, size_t> &fw1,
+                       const pair<string, size_t> &fw2) {
+      auto it = search(fw1.first.begin(), fw1.first.end(), fw2.first.begin(),
+                       fw2.first.end(), [](char c1, char c2) {
+                         return tolower(c1) == tolower(c2);
+                       });
+      return it == fw1.first.end();
+    };
 
-    // cout << (first == frequencies_words.begin() ? first->second : "no") <<
-    // endl;
+    auto contains = [](const pair<string, size_t> &fw1,
+                           const pair<string, size_t> &fw2) {
+      auto it = search(fw1.first.begin(), fw1.first.end(), fw2.first.begin(),
+                       fw2.first.end(), [](char c1, char c2) {
+                         return tolower(c1) == tolower(c2);
+                       });
+      return it != fw1.first.end();
+    };
 
-    for_each(
-        frequencies_words.begin(), frequencies_words.end(),
-        [user_input](const auto &fw) {
-          auto it = search(fw.second.begin(), fw.second.end(), user_input.begin(),
-                 user_input.end(),
-                 [](char c1, char c2) { return tolower(c1) == tolower(c2); });
-          cout << (it != fw.second.end() ? "sim" : "nao") << endl;
-        });
+    auto first = lower_bound(frequencies_words.begin(), frequencies_words.end(),
+                             pair<string, size_t>{user_input, 0}, not_contains);
+    auto last = lower_bound(frequencies_words.begin(), frequencies_words.end(),
+                            pair<string, size_t>{user_input, -1}, contains);
+
+
+
+    for_each(first, last, [](const auto &fw) { cout << fw.first << endl; });
+    // cout << (first != frequencies_words.end() ? first->first : "no") << " F"
+    //      << endl;
+    // cout << (last != frequencies_words.end() ? last->first : "no") << " L"
+    //      << endl;
   }
 }
