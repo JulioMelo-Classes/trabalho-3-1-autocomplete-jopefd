666f71b2e5ea2f143790519fb7efc2f134f40a6c
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-23 16:23:44 -0300

    Use Database and WordsFrequencies types

--- autocomplete/database_controller.hpp
+++ autocomplete/database_controller.hpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <set>
 
+#include "database.hpp"
+#include "words_frequencies.hpp"
+
 class DatabaseController {
  public:
-  void ProcessDatabase(const std::ifstream &database);
+  void ProcessDatabase(const Database &database);
 
-  std::set<std::pair<std::string, std::size_t>> get_words_frequencies() const;
+  WordsFrequencies get_words_frequencies() const;
 
-  void set_words_frequencies(const std::set<std::pair<std::string, std::size_t>>
-                                 &words_frequencies) const;
+  void set_words_frequencies(const WordsFrequencies &words_frequencies) const;
 
  private:
-  std::set<std::pair<std::string, std::size_t>> words_frequencies;
+  WordsFrequencies words_frequencies_;
 };
