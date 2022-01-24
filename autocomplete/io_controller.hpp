8baad6b1fe8ef56442b560254cb8f13da363cc2d
João Pedro F. Dantas <fonsecajope@gmail.com>
2022-01-24 17:01:49 -0300

    Use Database type

--- autocomplete/io_controller.hpp
+++ autocomplete/io_controller.hpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include "database.hpp"
+
 class IOController {
  public:
-  static std::ifstream OpenDatabase(const int &argc, const char **argv);
+  static Database OpenDatabase(const int &argc, const char **argv);
   static std::string ReadUserInput();
 };
